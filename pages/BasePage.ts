5faae4c72b2be6499ad8de935aefd6259836eeb6
Jyoti Sahoo <jyotishankarsahoo86@gmail.com>
2025-10-20 11:10:29 -0500

    Base Page update to have more re-usable methods

--- pages/BasePage.ts
+++ pages/BasePage.ts
@@ -1,4 +1,6 @@
-import {FrameLocator, Locator, Page } from "playwright";
+import { expect } from "../Utils/test-utilities";
+import { FrameLocator, Locator, Page } from "playwright";
+
 export class BasePage {
     protected static page: Page;
     // Static Pattern
@@ -15,5 +17,14 @@ export class BasePage {
     }
     protected frameLocator(selector: string): FrameLocator {
         return BasePage.page.frameLocator(selector);
+    }
+    protected async clickElement(locator: Locator): Promise<void> {
+        await expect(locator).toBeVisible();
+        await expect(locator).toBeEnabled();
+        await locator.click();
+    }
+    protected async fillField(locator: Locator, text: string): Promise<void> {
+        await expect(locator).toBeEditable();
+        await locator.fill(text);
     }
 }
